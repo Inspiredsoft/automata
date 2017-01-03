c472d297aa1bb7a9cb77190f51f12dc2b03b37c7
Gotamo <massimoromano3@gmail.com>
2017-01-03 17:45:22 +0100

    license

--- src/main/java/it/inspired/automata/WorkflowManagerImpl.java
+++ src/main/java/it/inspired/automata/WorkflowManagerImpl.java
@@ -1,3 +1,21 @@
+/*******************************************************************************
+* Inspired Automata is a framework implement a Finite State machine.
+* Copyright (C) 2017 Inspired Soft
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.    
+*******************************************************************************/
+
 package it.inspired.automata;
 
 import groovy.lang.Binding;
@@ -212,9 +230,10 @@ public class WorkflowManagerImpl implements WorkflowManager {
 				.getActualTypeArguments()[0];
 		
 			try {
-				return (E) Class.forName(type.getTypeName()).newInstance();
-			} catch (InstantiationException | IllegalAccessException
-					| ClassNotFoundException e) {
+				return (E) ((Class<?>) type).newInstance();
+			} catch (InstantiationException e) {
+				log.error("Error creating history item", e);
+			} catch (IllegalAccessException e) {
 				log.error("Error creating history item", e);
 			}
 		
